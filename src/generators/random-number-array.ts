092f2eb0fbf4283579817814fbc3efb0c4a7f241
shvmeless <shvme.development@gmail.com>
2022-07-11 16:05:20 -0400

    test: improve the 'random-number-array' generator tests

---
+++ src/generators/random-number-array.ts
@@ -0,0 +1,25 @@
+
+// FUNCTION
+export default function randomNumberArray (
+  params: { min: number, max: number, length: number }
+): number[] {
+
+  const min = Math.min(params.min, params.max)
+  const max = Math.max(params.min, params.max)
+
+  const length = params.length
+  const range = Math.abs(max - min)
+
+  const array: number[] = []
+
+  while (array.length < length) {
+
+    const random = Math.random() * range + min
+
+    array.push(random)
+
+  }
+
+  return array
+
+}
